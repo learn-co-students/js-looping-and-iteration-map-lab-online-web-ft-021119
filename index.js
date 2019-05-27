bf43d51d16bb0ce7b6a0093d6020a75ee3b9167a
Christoph Gruener <christophgruener@me.com>
2019-05-27 17:28:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  let newDrivers = drivers.map(function(drvr){
+    return drvr.toLowerCase();
+  })
+  return newDrivers;
+}
+
+function nameToAttributes(drivers) {
+  let newDrivers = drivers.map(function(drvr){
+    let driver = {};
+    driver["firstName"] = drvr.split(" ")[0];
+    driver["lastName"] = drvr.split(" ")[1];
+    return driver;
+  });
+  return newDrivers;
+}
+
+function attributesToPhrase(drivers) {
+  let announcement = drivers.map(function(drvr){
+    return `${drvr.name} is from ${drvr.hometown}`;
+  })
+  return announcement;
+}
